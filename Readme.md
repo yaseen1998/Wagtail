2f9e633c12099c74e73d3b3835f1ac9f6c685ca8
yaseen1998 <Yasseenkouthe@ENG.HU.EDU.JO>
2022-04-18 00:58:46 +0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+pip install wagtail
+wagtail start mysite
+cd mysite
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver
+
+
+https://github.com/wagtail/wagtail
---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-z3-bfs)94rn63@e%q7y9l16g3haw^)i(q7c(xo_*q!_1giksbt'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
